0187c07988a936bebb17120b7af99aa2cbeb4737
jakobveselsky <jveselsky@luc.edu>
2021-12-04 12:59:59 -0600

    sample code from opencv for stereo calibration and depth map creation

---
+++ CMakeFiles/generate_depth_map.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generate_depth_map.dir/depthMap.cpp.o"
+  "CMakeFiles/generate_depth_map.dir/depthMap.cpp.o.d"
+  "generate_depth_map"
+  "generate_depth_map.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/generate_depth_map.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
